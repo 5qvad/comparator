11e4aae8bbc467c176990ce623ae0fff9a1f03c0
Alexandr Goncharov <bitproton@gmail.com>
2022-08-31 15:04:10 +0300

    dell add removeIf

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,22 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
-        persons.add(new Person("Nikita", "Vlasov Petrov", 23));
-        persons.add(new Person("Maria", "Lesheva ", 25));
+        persons.add(new Person("Nikita", "Vlasov Petrov", 15));
+        persons.add(new Person("Maria", "Lesheva ", 11));
         persons.add(new Person("Andrew", "Durov Perviy Mstitel", 18));
-        persons.add(new Person("John", "Bikov Acme Tnt", 33));
+        persons.add(new Person("John", "Bikov Acme Tnt", 12));
         persons.add(new Person("Alex", "Goncharov Pablo", 35));
-        persons.add(new Person("Sasha", "Muraeva Tregukrudova Veronika Petrovna", 45));
+        persons.add(new Person("Sasha", "Muraeva Tregukrudova Veronika Petrovna", 17));
         persons.add(new Person("Dmitriy", "Pascal Don Kihot ", 20));
 
         System.out.println(persons);
 
         persons.sort(new PersonComparator(3));
 
+        persons.removeIf(a -> a.getAge()<18);
+
         System.out.println(persons);
     }
+
+
 }
